61c8cf60c7726399b9f36ec3c20905bfd5ec097a
khadarhassan <khadar05@hotmail.de>
2018-10-19 21:22:42 +0200

    added gulpfile and created sass task, removed unneeded dependencies and created src folder to store css

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify');
+const autoprefixer = require('gulp-autoprefixer');
+
+gulp.task('sass', function () {
+  return gulp.src(['./src/scss/app.scss'])
+    .pipe(sass().on('error', sass.logError))
+    .pipe(sass({
+      outputStyle: 'compressed'
+    }))
+    .pipe(concat('app.css'))
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions', 'ie >= 9']
+    }))
+    .pipe(gulp.dest('./static/css'));
+});
+
+// gulp.task('minify', function () {
+//   return gulp.src([
+//       './node_modules/jquery/dist/jquery.min.js',
+//       './node_modules/gsap/src/minified/easing/EasePack.min.js',
+//       './node_modules/gsap/src/minified/TweenLite.min.js',
+//       './src/js/app.js'
+//     ])
+//     .pipe(concat('app.js'))
+//     .pipe(uglify())
+//     .pipe(gulp.dest('./static/js'))
+// });
+
+gulp.task('watch', function () {
+  gulp.watch('./src/**/*', ['sass, minify']);
+});
